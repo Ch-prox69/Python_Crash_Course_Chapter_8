5b7abd1d8c8db88af8974145dbc86f704e0a346e
Christian Hartzell <144137283+Ch-prox69@users.noreply.github.com>
2024-10-07 11:52:30 -0400

    Add files via upload

---
+++ Hartzell_TIY_8.5.py
@@ -0,0 +1,18 @@
+def describe_city(city_name='Fort Wayne', city_country='usa'):
+    """Print message on users favorite city and its location"""
+    print(f"\nMy favorite city is {city_name.title()}, which is in {city_country.title()}.")
+
+# Space
+describe_city()
+
+# City 1
+describe_city(city_name='Indianapolis')
+
+# City 2
+describe_city(city_name='New York City')
+
+# City 3
+describe_city(city_name='London', city_country='UK')
+
+
+
